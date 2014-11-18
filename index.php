5108ac25418c48ec5efee8d1633f559f6d313e97
fusionPT <gigafusion@gmail.com>
2014-11-18 10:16:20 +0100

    Add analytics code to head of page

--- index.php
+++ index.php
@@ -25,7 +25,16 @@
 
   <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
   <!-- <script src="js/libs/modernizr-1.7.min.js"></script> -->
+ <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-41351971-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 
 <body>
@@ -223,17 +232,6 @@
     <script>DD_belatedPNG.fix("img, .png_bg"); // Fix any <img> or .png_bg bg-images. Also, please read goo.gl/mZiyb </script>
   <![endif]-->
 
-
- <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-41351971-3', 'auto');
-  ga('send', 'pageview');
-
-</script>
 
 <div id="fb-root"></div>
 
