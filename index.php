5b2e5c2992f3c19967d7927ecd896cca22852268
fusionPT <gigafusion@gmail.com>
2014-11-18 10:11:22 +0100

    Change to analytics code

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
   <title>Factura Rapida</title>
-  <meta name="description" content="Facturación on-line gratis en Español, Inglés y Portugués">
-  <meta name="author" content="Facturación on-line gratis. Facturarapida.net">
+  <meta name="description" content="Online Invoicing FREE. No Signup.">
+  <meta name="author" content="brunofelicio.com">
   <meta name="keywords" content="Invoices, free, billing, pdf, facturas, gratis, facturación, facturas en pdf, facturación en la nube">
 
   <!-- Mobile viewport optimized: j.mp/bplateviewport -->
@@ -224,17 +224,19 @@
   <![endif]-->
 
 
-  <script>
+ <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-41351971-3', 'facturarapida.net');
+  ga('create', 'UA-41351971-3', 'auto');
   ga('send', 'pageview');
 
 </script>
+
 <div id="fb-root"></div>
+
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
