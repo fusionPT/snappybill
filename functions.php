75f4ada21c8f53326a7f1bb9c3ae7b467fde3ce5
Bruno Felicio <bruno@brunofelicio.com>
2017-09-12 10:12:06 +0200

    blank spaces

--- functions.php
+++ functions.php
@@ -1,22 +1,19 @@
-<?php 
+<?php
 
 $items = array();
 
 function sendData($valor){
 
-		
+
 
 		foreach($valor as $item){
 
 			$items[] = $item;
-			
+
 		}
-		
-		
+
+
 	}
-
-
-
 
 function renderPDF($content_var){
 
@@ -24,23 +21,23 @@ function renderPDF($content_var){
 	{
 		$factura = $_POST['invoice-title'];
 		$invoice_num = $_POST['invoice-number'];
-		
+
 		$content = mb_convert_encoding($content_var, 'HTML-ENTITIES','UTF-8');
 		if(empty($content))
 		{
 			$error = "No fue posible renderizar el PDF!";
 		}
-		
+
 		else {
-		
+
 			include_once('dompdf/dompdf_config.inc.php');
-			
-				
+
+
 			$dompdf = new DOMPDF();
 			$dompdf->load_html(utf8_decode($content));
 			$dompdf->render();
 			$dompdf->stream($factura .'-'.$invoice_num. '.pdf');
-			
+
 		}
 	}
 }
